8adf71e21e7431ca3dcbb456b137a7dabc45d1ba
Brandon VanCamp <brandon@overtimewebdev.com>
2018-04-02 18:43:16 -0500

    n/a

--- js/App.js
+++ js/App.js
@@ -351,60 +351,39 @@ const handleSelect = (isTruthy, div) => {
 // ====================================================================
 // ---------		Section 5	 @ Payment Info > Button	-------------------
 // ====================================================================
-
-// const activitiesError = () => {
-// 	const parentNode = nodes.form.querySelector('.activities');
-// 	// const child = parentNode.querySelector('LABEL');
-// 	// const parentNode = nodes.findParent();
-// 	const HTML = `
-// 		<span style="position: absolute; top: 2rem; right: 0; border-bottom: 1px solid red; font-weight: bolder;">You must select at leaset one activity</span>
-// 	`;
-// 	parentNode.innerHTML = `${parentNode.innerHTML} ${HTML}`;
-// 	parentNode.setAttribute('style', 'position: relative;');
-// };
-
-
 const onSubmit = (e) => {
 	const url = nodes.url;
 	const basicInfo = nodes.fieldset[0].querySelectorAll('INPUT');
 	const activities = nodes.fieldset[2].querySelectorAll('INPUT');
 	const paymentType = nodes.fieldset[3].querySelector('#payment');
 
-	let toSubmit = false;
-	nodes.form.action = 'index.html';
-	// nodes.form.submit(); //form submission
 	nodes.attempts++;
-
-	let isValid = 0;
 
 	if (nodes.total > 0) { // Activies section check
 		handleRequiredFields(activities, null); // Remove required fields.
 		activities[1].parentNode.parentNode.firstElementChild.innerHTML = 'Register for Activities ';
-		isValid++;
 	} else {
 		handleRequiredFields(activities, null, true); // Add required fields.
 		activities[1].parentNode.parentNode.firstElementChild.innerHTML = nodes.notifyUser({
 			name: '--Please select at least one.',
 			before: 'Register for Activities'
 		});
+		return false;
 	}
 	if (paymentType.value === 'select_method') {
 		nodes.typeError(paymentType, errorTypes(.5).error);
+		return false;
 	} else {
 		nodes.typeError(paymentType, errorTypes(0).success);
-		isValid++;
 	}
 	if (url === undefined) {
 		handleRequiredFields(nodes.fieldset[3].querySelectorAll('INPUT'), 0);
-		isValid++;
 	} else {
 		const isCC = url !== null;
 		isCC ? window.open(nodes.url, '_blank') : nodes.typeError(paymentType, errorTypes(.5).error);
-		isValid++;
 	}
-
-	toSubmit = true;
-	return toSubmit;
+	nodes.form.action = 'index.html';
+	nodes.form.submit(); //form submission
 };
 
 
