f1d4d3b37ac324f3abb895b2ed8f4f1d0d78dbb6
Brandon VanCamp <brandon@overtimewebdev.com>
2018-04-02 16:11:37 -0500

    Clean / fix

--- js/App.js
+++ js/App.js
@@ -7,12 +7,21 @@
 //^ 	3.
 ==========================================================================================================*/
 "use strict";
-
 window.onload = () => { // On load select input form. === first;
 	const input = nodes.form.querySelector('input'); // Get first input element within form.
-	const firstDiv = nodes.fieldset[3].querySelector('DIV').style.display = 'none'; // Hide the payment div
 	input.focus(); // Focus input.
 	input.select(); // Then select Element.
+	const ccDiv = nodes.fieldset[3].querySelector('DIV'); // Select CC Div.
+	ccDiv.style.display = 'none' // Hide the payment divs
+	const payPal = ccDiv.nextElementSibling;
+	payPal.style.display = 'none'; // Hide the payment divs
+	const bitCoin = payPal.nextElementSibling.style.display = 'none'; // Hide the payment divs
+
+	const basicInfo = document.querySelectorAll('INPUT');
+	const HTML = `
+	<span class="validity"></span>`;
+	basicInfo[0].outerHTML = `${basicInfo[0].outerHTML} ${HTML}`;
+	basicInfo[1].outerHTML = `${basicInfo[1].outerHTML} ${HTML}`;
 };
 const nodeConfig = (f) => { // Traverse the DOM to select needed nodes.
 	const form = document.querySelector('FORM');
@@ -55,10 +64,16 @@ const nodeConfig = (f) => { // Traverse the DOM to select needed nodes.
 			const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 			return re.test(String(email).toLowerCase());
 		},
+		// isChecked(inputValue) {
+		// 	this.inputValue = inputValue;
+		// 	const isValid = inputValue.value.length >= 2;
+		// 	return (isValid ? true : false);
+		// },
 		total: 0,
 		button,
 		allInput,
-		url: null
+		url: null,
+		attempts: 0
 	};
 	return exportNodes;
 };
@@ -84,14 +99,21 @@ const onLoad = (node) => { // Create on load.
 	nodes.button.type = 'button';
 }
 
-const handleRequiredFields = (inputs, num = null, isTrue) => { // Mainly for CC payments.
+const handleRequiredFields = (inputs, num = null, isTrue = false) => { // Mainly for CC payments.
 	for (var i = 0; i < inputs.length - num; i++) {
 		if (nodes.isValue(inputs[i])) {
 			inputs[i].removeAttribute('required', true);
-			nodes.typeError(inputs[i], errorTypes(.3).success);
+			nodes.typeError(inputs[i], errorTypes(.5).success);
 		} else {
 			inputs[i].setAttribute('required', true);
-			nodes.typeError(inputs[i], errorTypes(.3).error);
+			nodes.typeError(inputs[i], errorTypes(.5).error);
+		}
+		if (inputs[i].type === 'checkbox' && isTrue) {
+			// console.log(inputs[i]);
+			inputs[i].setAttribute('required', true);
+		} else if (inputs[i].type === 'checkbox') {
+			console.log(inputs[i]);
+			inputs[i].removeAttribute('required');
 		}
 	}
 }
@@ -101,17 +123,21 @@ const handleRequiredFields = (inputs, num = null, isTrue) => { // Mainly for CC 
 const otherJobRoleField = (parentNode) => { // Create and append new input if other is selected.
 	const HTML = `
 	<label for="other-title">Other Job Role</label>
-	<input type="text" id="other-title" name="user_job" placeholder="Your Job Role">`;
+	<input type="text" id="other-title" name="user_job" placeholder="Your Job Role" required>`;
 	nodes.mkNode('DIV', parentNode, `${HTML}`);
 };
 const jobRoleSelection = (e) => {
 	let optionOther = e.target.value === 'other';
 	let appendedDiv = nodes.fieldset[0].lastElementChild;
-	if (optionOther) {
-		otherJobRoleField(nodes.fieldset[0]); // Pass 1st fieldset DOM node.
-	} else if (appendedDiv.tagName === 'DIV') {
-		appendedDiv.remove(); // Remove old Div
+
+	if (e.target.tagName === 'SELECT') { // Double check we are only fireing on SELECT NODES values.
+		if (optionOther) {
+			otherJobRoleField(nodes.fieldset[0]); // Pass 1st fieldset DOM node.
+		} else if (appendedDiv.tagName === 'DIV') {
+			appendedDiv.remove(); // Remove old Div
+		}
 	}
+
 };
 // ===========================================================
 // ---------		Section 2	 	@T-Shirt Info		------------------
@@ -221,7 +247,7 @@ const handleActivities = (e) => {
 // ---------		Section 4	 	@ Payment Info		------------------
 // =============================================================
 const handleCcSelection = (e) => {
-	const firstDiv = nodes.fieldset[3].querySelector('DIV');
+	const paymentDivs = nodes.fieldset[3].querySelectorAll('DIV');
 	const select = nodes.fieldset[3].querySelector('SELECT');
 	const isCC = select.value === 'credit card'; // CreditCard
 	const isPP = select.value === 'paypal'; // PayPal
@@ -231,58 +257,130 @@ const handleCcSelection = (e) => {
 		isPP,
 		isBC
 	};
-	handleSelect(toCheck, firstDiv);
+	handleSelect(toCheck, paymentDivs);
+};
+const handleSelect = (isTruthy, div) => {
+	div[0].style.display = 'none';
+	div[4].style.display = 'none';
+	div[5].style.display = 'none';
+
+	if (isTruthy.isCC) {
+		div[0].style.display = 'block';
+		nodes.url = undefined;
+
+		div[1].querySelector('INPUT').setAttribute('maxlength', '19');
+		div[1].querySelector('INPUT').setAttribute('minlength', '12');
+
+		div[2].querySelector('INPUT').setAttribute('maxlength', '5');
+		div[2].querySelector('INPUT').setAttribute('minlength', '5');
+
+		div[3].querySelector('INPUT').setAttribute('maxlength', '3');
+		div[3].querySelector('INPUT').setAttribute('minlength', '3');
+
+		for (var i = 1; i < div.length - 2; i++) {
+			if (parseInt(div[i].querySelector('INPUT').value)) {
+				nodes.typeError(div[i].querySelector('INPUT'), errorTypes(.5).success);
+
+				if (div[i].querySelector('INPUT').value.length >= div[i].querySelector('INPUT').minLength) {
+					nodes.typeError(div[i].querySelector('INPUT'), errorTypes(.5).success);
+				} else {
+					nodes.typeError(div[i].querySelector('INPUT'), errorTypes(.5).error);
+				}
+			} else if (typeof div[i].querySelector('INPUT').value === 'string' && div[i].querySelector('INPUT').value.length > 1) {
+				nodes.typeError(div[i].querySelector('INPUT'), errorTypes(.5).error);
+				div[i].querySelector('INPUT').value = '';
+			} else {
+				div[i].querySelector('INPUT').value = '';
+			}
+		}
+
+	} else if (isTruthy.isPP) {
+		const url = 'https://www.paypal.com/us/home';
+		div[4].style.display = 'block';
+		nodes.url = url;
+	} else
+	if (isTruthy.isBC) {
+		const url = 'https://bitcoin.org/en/';
+		div[5].style.display = 'block';
+		nodes.url = url;
+	}
 };
 // ====================================================================
 // ---------		Section 5	 @ Payment Info > Button	-------------------
 // ====================================================================
-const handleSelect = (isTruthy, div) => {
-	if (isTruthy.isCC) {
-		div.style.display = 'block';
-		nodes.url = undefined;
-	} else if (isTruthy.isPP) {
-		const url = 'https://www.paypal.com/us/home';
-		nodes.url = url;
-		div.style.display = 'none';
-	} else if (isTruthy.isBC) {
-		const url = 'https://bitcoin.org/en/';
-		nodes.url = url;
-		div.style.display = 'none';
-	} else {
-		div.style.display = 'none';
-	}
-};
-const onSubmit = (e) => {
-	// e.preventDefault();
-	const url = nodes.url;
 
+const activitiesError = () => {
+	const parentNode = nodes.form.querySelector('.activities');
+	// const child = parentNode.querySelector('LABEL');
+	// const parentNode = nodes.findParent();
+	const HTML = `
+		<span style=" position: absolute; top: 2rem; right: 0; border-bottom: 1px solid red; font-weight: bolder;">You must select at leaset one activity</span>
+	`;
+	console.log();
+
+	parentNode.innerHTML = `${parentNode.innerHTML} ${HTML}`;
+	parentNode.setAttribute('style', 'position: relative;');
+
+	// const newNode = nodes.mkNode('DIV', parentNode, `${HTML}`);
+	// parentNode.insertBefore(newNode, child);
+	// parentNode.setAttribute('style', 'position: relative;');
+};
+
+
+const onSubmit = (e) => {
+	const url = nodes.url;
 	const basicInfo = nodes.fieldset[0].querySelectorAll('INPUT');
 	const activities = nodes.fieldset[2].querySelectorAll('INPUT');
+	const paymentType = nodes.fieldset[3].querySelector('#payment');
+
+	console.log(paymentType);
 
 
-	handleRequiredFields(basicInfo, null, false); // Add required fields.
 
-	if (nodes.total > 0) {
-		handleRequiredFields(activities, null, true); // Remove required fields.
+	// if (!basicInfo[0].validity.valid) {
+	nodes.form.action = 'index.html';
+	// nodes.button.type = 'submit';
+	nodes.attempts++;
+	// }
+	// else {
+
+	activitiesError();
+
+	if (nodes.total > 0) { // Activies section check
+		handleRequiredFields(activities, null); // Remove required fields.
 	} else {
-		handleRequiredFields(activities, null, false); // Add required fields.
-		console.log('NOOO');
+		handleRequiredFields(activities, null, true); // Add required fields.
 	}
 
-	// console.log(nodes.validateEmail(basicInfo[1].value));
+	if (nodes.validateEmail(basicInfo[1].value)) {
+		nodes.typeError(basicInfo[1], errorTypes(.5).success);
+	} else {
+		// NOTE: Create new span for valid or invalid content notification.
+		nodes.typeError(basicInfo[1], errorTypes(.5).error);
+	}
 
+	if (paymentType.value === 'select_method') {
+		nodes.typeError(paymentType, errorTypes(.5).error);
+		nodes.button.type = 'button';
+	} else {
+		nodes.typeError(paymentType, errorTypes(0).success);
+	}
 
 
 	if (url === undefined) {
-		handleRequiredFields(nodes.allInput, 0);
+		handleRequiredFields(nodes.fieldset[3].querySelectorAll('INPUT'), 0);
+	} else {
+		const isCC = url !== null;
+		// isCC ? window.open(nodes.url, '_blank') : alert('select a payment type.');
 	}
-	// else {
-	// const isCC = url !== null;
-	// isCC ? window.open(nodes.url, '_blank') : alert('select a payment type.');
-	// }
+
+	// activitiesError();
+
+
 	// NOTE: Not sure about this....
-	// nodes.form.method = 'POST'
-	// nodes.button.type = 'submit';
+
+	console.log(nodes.button.type);
+	// }
 	console.log(nodes.button.type);
 };
 
